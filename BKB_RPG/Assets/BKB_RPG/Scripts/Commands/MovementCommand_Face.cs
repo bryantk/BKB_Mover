b32d22dbdd2d1ec4ccd97e0d7979369ca294682f
bryant.kyle.d@gmail.com <iknowspallgud@gmail.com>
2016-01-13 21:22:30 -0700

    Master Rebase

---
+++ BKB_RPG/Assets/BKB_RPG/Scripts/Commands/MovementCommand_Face.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+namespace BKB_RPG {
+    [System.Serializable]
+    public class MovementCommand_Face : MovementCommand_Move {
+
+        public MovementCommand_Face() : base() {
+            command_type = CommandTypes.Face;
+            facingCommand = true;
+        }
+
+    }
+}
