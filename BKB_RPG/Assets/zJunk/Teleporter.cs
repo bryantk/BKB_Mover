4ff66465afd60a0c4a6536405546672e7b8cf951
bryant.kyle.d@gmail.com <iknowspallgud@gmail.com>
2016-11-29 21:08:04 -0700

    keeping Temp asset folder

---
+++ BKB_RPG/Assets/zJunk/Teleporter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using BKB_RPG;
+
+[RequireComponent(typeof(Collider2D))]
+public class Teleporter : MonoBehaviour {
+
+    public string label;
+    public Vector3 position;
+
+    
+    public string scene;
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Player p = other.GetComponent<Player>();
+        if (p != null)
+            return;
+       Teleport();
+    }
+
+    public void Teleport() {
+        scene = scene != "" ? scene : null;
+        if (label.Contains("."))
+        {
+            GameMaster.Teleport(label);
+            return;
+        }
+        if (label != "")
+            GameMaster.Teleport(label, scene);
+        else
+            GameMaster.Teleport(position, scene);
+    }
+
+}
